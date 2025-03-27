ff844f8c9be0e8733c0a2ff4c6556c6bd3906e23
David <dmonaresss@gmail.com>
2025-03-27 17:01:41 +0300

    Create Header

--- components/header/index.vue
+++ components/header/index.vue
@@ -1,45 +1,82 @@
 <template>
-	<div class="container">
+	<div>
 		<header class="header">
-			<div class="header__logo">
-				<icons-logo />
-			</div>
-			<nav class="header__navigation">
-				<NuxtLink class="header__navigation-link" to="/">
-					Главная
-				</NuxtLink>
+			<nav class="header__nav">
+				<ul class="header__nav-list">
+					<div class="header__nav-group">
+						<li class="header__nav-item">
+							<nuxt-link class="header__nav-link" to="/">
+								Shop
+							</nuxt-link>
+						</li>
+						<li class="header__nav-item">
+							<nuxt-link class="header__nav-link" to="/">
+								Always Pan
+							</nuxt-link>
+						</li>
+					</div>
+					<li class="header__nav-logo">
+						<nuxt-link class="header__nav-link" to="/">
+							<icons-logo />
+						</nuxt-link>
+					</li>
+					<div class="header__nav-group">
+						<li class="header__nav-item">
+							<nuxt-link class="header__nav-link" to="/">
+								Mission
+							</nuxt-link>
+						</li>
+						<li class="header__nav-item">
+							<nuxt-link class="header__nav-link" to="/">
+								FAQs
+							</nuxt-link>
+						</li>
+					</div>
+				</ul>
 			</nav>
+			<div class="header__user">
+				<icons-user />
+			</div>
 		</header>
 	</div>
 </template>
 
-<style lang='scss'>
-	.header
-	{
-		margin-bottom: 50px;
-		padding: 20px 0;
-		border-bottom: 2px solid $black;
+<style lang="scss">
+.header {
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	padding: 25px 48px;
+	gap: 25px;
 
-		display: flex;
-		align-items: center;
-		justify-content: space-between;
+	&__nav {
+		flex-grow: 1;
+
+		&-list {
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+			width: 100%;
+		}
+
+		&-item { cursor: pointer; }
+
+		&-link {
+			color: $black;
+			font-size: 13px;
+			font-weight: 700;
+			text-transform: uppercase;
+			transition: color 0.3s;
+
+			&:hover {
+				color: $brown;
+			}
+		}
+
+		&-group {
+			display: flex;
+			gap: 35px;
+		}
 	}
-
-	.header__logo { line-height: 0; }
-
-	.header__navigation
-	{
-		display: flex;
-		align-items: center;
-		gap: 25px;
-	}
-
-	.header__navigation-link
-	{
-		font-size: 1.125rem; // 18px
-		color: $gray;
-
-		@include transition();
-		&.router-link-active { color: $black; }
-	}
+}
 </style>
